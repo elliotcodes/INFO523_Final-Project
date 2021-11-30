6ed66cc58be24ec60c915d13a4dac706a88d298e
Jung Mee Park <65413883+jmp243@users.noreply.github.com>
2021-11-30 11:04:52 -0700

    changes to arrest number
    I believe I initially calculated the arrest scores incorrectly, driving up the number of arrests.

--- R-Files/server.R
+++ R-Files/server.R
@@ -106,9 +106,13 @@ server <- function(input, output, session) {
     # # load in data
     load("Dataset/info523-CVRM_Subset-02.RData")
     CVRM_subset$distlastresidence <- as.character(CVRM_subset$distlastresidence)
-
+    # calculate correct arrest numbers
+    CVRM_subset <- CVRM_subset %>%
+        group_by(distlastresidence) %>%
+        mutate(COUNT_arr = sum(arrest_num))
+    
     districts_raw <- sf::read_sf("Dataset/Boundaries - Police Districts (current).geojson")
-
+    
     CVRM_subset1 <- left_join(districts_raw, CVRM_subset, by = c("dist_num" = "distlastresidence"))
 
     # subset data to remove 31
@@ -116,22 +120,22 @@ server <- function(input, output, session) {
         group_by(dist_num) %>%
         dplyr::filter(dist_num != "31")
   
-      # # try choropleth for non 31
-    CVRM_no31 <- CVRM_no31 %>%
-        group_by(arrest_num) %>%
-        mutate(COUNT = n())
+    #   # # try choropleth for non 31
+    # CVRM_no31 <- CVRM_no31 %>%
+    #     group_by(arrest_num) %>%
+    #     mutate(COUNT = n())
 
     # bins for the number of arrests
-    bins <- c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, Inf)
+    bins <- c(0, 10, 20, 30, 40, 50, Inf)
     
     # color palette
-    pal <- colorBin("Blues", domain = CVRM_no31$COUNT, bins = bins)
+    pal <- colorBin("Blues", domain = CVRM_no31$COUNT_arr, bins = bins)
     
     # labels for the map
     # labels <- reactive({sprintf(
     labels <- sprintf(
         "<strong>%s DISTRICT</strong><br/>%g arrests",
-        CVRM_no31$dist_label, CVRM_no31$COUNT
+        CVRM_no31$dist_label, CVRM_no31$COUNT_arr
     ) %>% lapply(htmltools::HTML)
     
     output$map <- renderLeaflet({
@@ -142,17 +146,19 @@ server <- function(input, output, session) {
             addMarkers(lng=-87.663045, lat=42.009932, popup="Chicago") %>%
             addMarkers(lng=-87.827286, lat=41.963364, label = "Norridge Village") %>% 
             setView(lng = -87.7539448, lat = 41.8455877, zoom = 11)  %>%
-            addPolygons(data = CVRM_no31, stroke = TRUE, smoothFactor = 0.2, opacity = 0.2, fillOpacity = 0.2,
-                        fillColor = ~pal(COUNT), color = "white", weight = 1,
-                        highlightOptions = highlightOptions(color = "white", weight = 2, fillOpacity = 0.1,
-                                                            bringToFront = TRUE),
+            addPolygons(data = CVRM_no31, stroke = TRUE, smoothFactor = 0.2, fillOpacity = 0.1,
+                        fillColor = ~pal(COUNT_arr), color = "white", weight = 1,
+                        highlightOptions = highlightOptions(
+                            # color = "white", weight = 2, 
+                            fillOpacity = 0.7,
+                            bringToFront = TRUE),
                         label = labels,
                         labelOptions = labelOptions(
                             style = list("font-weight" = "normal", padding = "3px 8px"),
                             textsize = "15px",
                             direction = "auto")
             ) %>%
-        addLegend("bottomleft", pal = pal, values = CVRM_no31$COUNT,
+        addLegend("bottomleft", pal = pal, values = CVRM_no31$COUNT_arr,
                   title = "Number of arrests",
                   opacity = 1)
     })
